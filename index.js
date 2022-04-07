d596ef1496f0acb4d1e6506219d5883fe49ee842
T-Tsava <t.tsava.websmart@gmail.com>
2022-04-07 16:29:34 +0400

    edit for deploy

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const routes = require('./routes/routes');
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.json());
 
 app.use( function(req, res, next) {
     if (req.originalUrl && req.originalUrl.split("/").pop() === 'favicon.ico') {
